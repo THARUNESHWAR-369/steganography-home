118c1e6fb651ba0d2cb3b0eaf5ba43d50931cd5b
THARUNESHWAR-369 <s.tharueshwar.369@gmail.com>
2024-04-12 02:21:31 +0530

    Image Stepic Completed

--- app.py
+++ app.py
@@ -6,28 +6,11 @@ from modes.Image.image import image
 from modes.Video.video import video
 
 
-UPLOAD_IMAGE_FOLDER = 'modes\\Image\\static'
-IMAGE_CACHE_FOLDER = 'modes\\Image\\__pycache__'
-UPLOAD_TEXT_FOLDER = 'modes\\Text\\static'
-TEXT_CACHE_FOLDER = 'modes\\Text\\__pycache__'
-UPLOAD_AUDIO_FOLDER = 'modes\\Audio\\static'
-AUDIO_CACHE_FOLDER = 'modes\\Audio\\__pycache__'
-UPLOAD_VIDEO_FOLDER = 'modes\\Video\\static'
-VIDEO_CACHE_FOLDER = 'modes\\Video\\__pycache__'
-
-
 # ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
-app.secret_key = "hello"
-app.config['UPLOAD_IMAGE_FOLDER'] = UPLOAD_IMAGE_FOLDER
-app.config['IMAGE_CACHE_FOLDER'] = IMAGE_CACHE_FOLDER
-app.config['UPLOAD_TEXT_FOLDER'] = UPLOAD_TEXT_FOLDER
-app.config['TEXT_CACHE_FOLDER'] = TEXT_CACHE_FOLDER
-app.config['UPLOAD_AUDIO_FOLDER'] = UPLOAD_AUDIO_FOLDER
-app.config['AUDIO_CACHE_FOLDER'] = AUDIO_CACHE_FOLDER
-app.config['UPLOAD_VIDEO_FOLDER'] = UPLOAD_VIDEO_FOLDER
-app.config['VIDEO_CACHE_FOLDER'] = VIDEO_CACHE_FOLDER
+app.secret_key = "<Your Key>"
+
 app.register_blueprint(image, url_prefix="/image")
 # app.register_blueprint(audio, url_prefix="/audio")
 # app.register_blueprint(text, url_prefix="/text")
